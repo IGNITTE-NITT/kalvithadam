c2201ea0a0a6f895be5991fac73333d327c5cbb2
IGNITTEADMIN <ignittetechnical@gmail.com>
2024-03-18 20:22:40 +0530

    TrialCommitByAnkitBhaiyya

--- src/careers/agriculture.js
+++ src/careers/agriculture.js
@@ -1,9 +1,118 @@
 import React from 'react'
+import Header from '../Header'
+import Footer from '../Footer'
 
 const agriculture = () => {
   return (
-    <div>
-      <h1>Hello Agriculture</h1>
+    <div className="container">
+      <br />
+      <Header />
+      <hr />
+      
+      <p>Agriculture has now emerged as one of the popular courses after grade 12. Agriculture has different courses ranging from UG courses like BTech, BSc, BS to Diploma courses. A few of the popular courses are Horticulture, Agricultiral Engineering, Forestry and Sericulture.</p>
+      <p>Agricultural courses aim to provide training to implement modern agricultural techniques and technologies in the real world scenario.</p>
+
+      <br />
+
+      <h3>OVERVIEW</h3>
+      <ul>
+        <li>ICAR AIEEA</li>
+        <li>TNAU</li>
+        <li>Top Colleges in TamilNadu</li>
+        <li>Top Colleges in India</li>
+      </ul>
+
+      <hr />
+
+      <h2>ICAR AIEEA</h2>
+
+      <br />
+
+      <h4>INTRODUCTION</h4>
+      <p>
+        ICAR AIEEA is the entrance exam for admission in undergraduate, postgraduate, and doctoral agriculture
+and allied sciences programs.
+        <br />
+        The AIEEA UG is the entrance exam for admission into undergraduate courses.
+      </p>
+
+      <br />
+
+      <h4>EXAM DETAILS</h4>
+      <ul>
+        <li><b>Exam Name:</b> AIEEA UG</li>
+        <li><b>Languages:</b> English, Hindi</li>
+        <li><b>Mode of Exam:</b> Online</li>
+        <li><b>Number of questions:</b> 150</li>
+        <ul>
+          <li>Stream1: 50 Questions each from Physics, Chemistry, Maths</li>
+          <li>Stream2: 50 Questions each from Physics, Chemistry, Biology / Agriculture</li>
+        </ul>
+        <li><b>Type of questions:</b> Objective Type</li>
+        <li><b>Total Marks:</b> 600 Marks</li>
+        <li><b>Marking Scheme:</b></li>
+        <ul>
+          <li>4 Marks for correct answer</li>
+          <li>-1 Marks for wrong answer</li>
+        </ul>
+        <li><b>Duration:</b> 2 Hours 30 minutes</li>
+      </ul>
+
+      <br />
+
+      <h4>ELIGIBLITY</h4>
+
+      <ul>
+        <li>Applicant must be an Indian citizen.</li>
+        <li>Applicant’s age must be 16 years as of August 31 (The year in which the exam is written).</li>
+        <li>Candidates must have studied Physics, Chemistry, and Biology/ Mathematics/Agriculture/Home Science in 10+2.</li>
+        <li>Those who wish to opt for stream-A in the entrance exam must have passed 10+2 with Physics, Chemistry, and Biology/Agriculture/Home Science subjects.</li>
+        <li>Those who wish to opt for stream-B have to pass 10+2 with Physics, Chemistry, and Mathematics subjects.</li>
+        <li>Applicants need to secure 50% aggregate marks in 10+2.</li>
+        <li>Reserved category candidates need to secure 40% aggregate marks in 10+2.</li>
+      </ul>
+
+      <br />
+
+      <h4>CONTACT DETAILS</h4>
+      <p>
+        <b>Website:</b> https://icar.nta.ac.in/
+        <br />
+        <b>Phone:</b> 011-69227700, 011-40759000
+      </p>
+
+      <hr />
+
+      <h2>TAMIL NADU AGRICULTURAL UNIVERSITY (TNAU)</h2>
+      
+      <br />
+
+      <h4>INTRODUCTION</h4>
+      <p>
+        Tamil Nadu Agricultural University (TNAU) is located in Coimbatore, Tamil Nadu, India and was established in the year 1971. The university is recognised by UGC and functions in accordance with the Tamil Nadu Directorate of Education.
+        <br />
+        The University is offering 13 Undergraduate Degree Programs, 29 Postgraduate Degree Programs and 26 Doctoral Programs in 14 Colleges distributed in 11 campuses all over Tamil Nadu.
+      </p>
+
+      <br />
+
+      <h4>COURSES OFFERED:</h4>
+      Undergraduate Courses:
+      <ul>
+        <li>B. Sc. (Hons.) Agriculture</li>
+        <li>B. Sc. (Hons.) Horticulture</li>
+        <li>B. Sc. (Hons.) Forestry</li>
+        <li>B. Sc. (Hons.) Food, Nutrition and Dietetics</li>
+        <li>B. Tech. (Agricultural Engineering)</li>
+        <li>B. Sc. (Hons.) Sericulture</li>
+        <li>B. Tech. (Food Technology)</li>
+        <li>B. Tech. (Biotechnology)</li>
+        <li>B. Tech. (Energy and Environmental Engineering)</li>
+        <li>B. S. (Agribusiness Management)</li>
+      </ul>
+
+      <hr />
+      <Footer />
     </div>
   )
 }
